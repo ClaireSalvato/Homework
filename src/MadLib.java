dfecc9707fd9fac60abb1e5d165f399c9c55b207
ClaireSalvato <claire_salvato26@milton.edu>
2023-10-28 18:11:12 -0400

    hehe

--- src/MadLib.java
+++ src/MadLib.java
@@ -7,6 +7,71 @@ public class MadLib {
     public MadLib(){
         System.out.println("hello world");
 
-        
+        String Claire = "Claire";
+        System.out.println(Claire);
+
+        String ToaPayoh = "ToaPayoh";
+        System.out.println(ToaPayoh);
+
+        String wallet = "wallet";
+        System.out.println(wallet);
+
+        String sunny = "sunny";
+        System.out.println(sunny);
+
+        String umbrellas = "umbrellas";
+        System.out.println(umbrellas);
+
+        String wildBoars = "wildBoars";
+        System.out.println(wildBoars);
+
+        String trueOrFalse = "true";
+        System.out.println(true);
+
+        String thirtyFive = "thirtyFive";
+        System.out.println(thirtyFive);
+
+        String fifty = "fifty";
+        System.out.println(fifty);
+
+        String sixteen = "sixteen";
+        System.out.println(sixteen);
+
+        String three = "3";
+        System.out.println(3);
+
+        String mxBradford = "mxBradford";
+        System.out.println(mxBradford);
+
+        String mrRuiz = "mrRuiz";
+        System.out.println(mrRuiz);
+
+        String Bplus= "Bplus";
+        System.out.println(Bplus);
+
+        String fall = "fall";
+        System.out.println(fall);
+
+        //define a string called story for mad lib
+        String story = "This weekend I am going out with "
+                +Claire+ ". We are going to a hawker centre in "
+                +ToaPayoh+ ". I packed my "+wallet+ " and "
+                +umbrellas + ".  The forecast is calling for a high of "
+                +thirtyFive+ " degrees celcius. So it should be a "
+                +sunny+" day! This year, the park rangers have seen "
+                +fifty+ " " +wildBoars + " which seems kind of dangerous. "
+                +mxBradford+" said it's " + trueOrFalse + " that "+wildBoars+ " eat class "
+                +three+ " students in the " +fall+ ". But, "
+                +mrRuiz+ " said it actually has only happened "
+                + sixteen+" times. Wish us luck! If we survive, we will earn a "
+                +Bplus+ "- in P.E. class.";
+
+//now print story to the dos window
+        System.out.println(story);
+
+
+
+
+
     }
 }
