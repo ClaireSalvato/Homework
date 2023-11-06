82d28b822dea6e30e1e78e93d9f428b9e084d3de
ClaireSalvato <claire_salvato26@milton.edu>
2023-11-06 11:49:33 -0500

    we corrected the brackets.

---
+++ src/Conditionals.java
@@ -0,0 +1,10 @@
+public class Conditionals {
+    public static void main(String[] args) {
+        Conditionals MagicEightBall= new Conditionals ();
+
+    } //main method
+
+    public Conditionals () {
+
+    } //constructor method
+}
